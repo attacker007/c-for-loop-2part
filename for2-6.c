0e59b225239d249656bd6838b6286c4bc6cadd7b
sutharsan manickam <sutharsansukgu@gmail.com>
2026-02-19 09:41:30 +0530

    Create for2-6.c

---
+++ for2-6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum=0;
+    scanf("%d",&a);
+    for(int i=0;i<a;i++){
+        scanf("%d",&b);
+        sum+=b;
+    }
+    printf("total collection ;%d",sum);
+}
