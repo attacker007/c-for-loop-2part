ccfae3e6cd6f3adfa384835ec4dde1649320fe8c
sutharsan manickam <sutharsansukgu@gmail.com>
2026-02-20 11:10:49 +0530

    Create for2-17.c

---
+++ for2-17.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,sum=0,count=0;
+    scanf("%d",&a);
+    for(int i=0;i<a;i++){
+        scanf("%d",&b);
+        sum+=b;
+        if(b>30)
+        count++;
+    }
+    printf("total delay%d\n",sum);
+    printf("delayed deliveries%d\n",count);
+    return 0;
+}
